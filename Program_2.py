89707a9b495ac4da19d7b765559cb2846fdf11e7
Pradeep Kumar <57650310+PradeepKumar2803@users.noreply.github.com>
2019-11-13 12:18:44 +0530

    Update Program_2.py
    Added actual output of the program

--- Program_2.py
+++ Program_2.py
@@ -4,3 +4,7 @@ c=9.5
 d=(a*b*c/100)
 e=int(d)
 print("Simple Interest for P {0}, n {1}, r {2} is {3}" .format(a,b,c,e))
+
+## Corresponing output entering here ##
+============== RESTART: E:\Python_Program\07-11-2019\Program_2.py ==============
+Simple Interest for P 10000, n 5, r 9.5 is 4750
